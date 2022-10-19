4ebe7fd6bc7ba44dce395f5c47694dff91f2f9b8
fadlan7 <inifadlan7@gmail.com>
2022-10-19 22:44:52 +0700

    create seeders for admin data

---
+++ seeders/20221019153927-seeding_create_admin_data.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          full_name: 'Admin',
+          email: 'admin@gmail.com',
+          password: '123456',
+          gender: 'male',
+          role: 1,
+        },
+      ],
+      {}
+    );
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {});
+  },
+};
