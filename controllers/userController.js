ae93f2741e65c5adda5c64f07f7199c1a1fe618f
fadlan7 <inifadlan7@gmail.com>
2022-10-20 12:52:43 +0700

    user login controller

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
 const { User } = require('../models');
 const currencyFormat = require('../helpers/currencyFormat');
+const { comparePassword } = require('../helpers/bcrypt');
+const { generateToken } = require('../helpers/jwt');
 class UserController {
   static async register(req, res) {
     const { full_name, password, gender, email } = req.body;
@@ -13,9 +15,7 @@ class UserController {
         role: roleValue,
       });
 
-      const userId = +userData.id;
       const userBalance = currencyFormat(userData.balance);
-      //   console.log(userBalance);
       const displayData = {
         id: +userData.id,
         full_name,
@@ -26,6 +26,47 @@ class UserController {
       };
 
       res.status(201).json({ user: displayData });
+    } catch (error) {
+      if (
+        error.name === 'SequelizeValidationError' ||
+        error.name === 'SequelizeUniqueConstraintError'
+      ) {
+        return res.status(400).json({
+          message: error.errors.map((e) => e.message),
+        });
+      }
+
+      return res.status(500).json({ message: error.message });
+    }
+  }
+
+  static async login(req, res) {
+    const { email, password } = req.body;
+
+    try {
+      const userData = await User.findOne({
+        where: {
+          email,
+        },
+      });
+
+      if (userData) {
+        const isCorrectPassword = comparePassword(password, userData.password);
+
+        if (isCorrectPassword) {
+          const token = generateToken({
+            id: userData.id,
+            email: userData.email,
+          });
+          res.status(200).json({ token });
+        } else {
+          res.status(401).json({ message: 'Wrong Password' });
+        }
+      } else {
+        res
+          .status(401)
+          .json({ message: `User with email ${email}  not found` });
+      }
     } catch (error) {
       if (
         error.name === 'SequelizeValidationError' ||
--- routers/index.js
+++ routers/index.js
@@ -2,5 +2,6 @@ const router = require('express').Router();
 const { UserController } = require('../controllers');
 
 router.post('/users/register', UserController.register);
+router.post('/users/login', UserController.login);
 
 module.exports = router;
